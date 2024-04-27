c9c440a4626e3f416230d8218a3d542310ba3257
Ramsey <17701825950@163.com>
2024-04-27 09:49:33 +0800

    change feishu

--- backtrader/feishu.py
+++ backtrader/feishu.py
@@ -1,49 +1,43 @@
 import requests
 import time
 
-code = "111222"
-timestamp = time.strftime("%Y-%m-%d-%H:%M", time.localtime())
-overview = f'**code**: {code}\n'
-header = {
-    "title":{
-        "tag":"plain_text",
-        "content": f'{timestamp}'
-        },
-    "template":"green"
-}
-
-def gen_json_text():
+def gen_json_text(code):
     json_text = {
         "msg_type": "interactive",
         "card": {
             "config": {
                 "wide_screen_mode":True
             },
-            "header": header,
+            "header": {
+                "title":{
+                    "tag":"plain_text",
+                    "content": f'{time.strftime("%Y-%m-%d-%H:%M", time.localtime())}'
+                    },
+                "template":"green"
+            },
             "elements":[
             {
                 "tag":"div",
                 "text":{
                     "tag":"lark_md",
-                    "content": f'{overview}'
+                    "content": f'**code**: {code}\n'
                 }
             }
             ]
         }
     }
-
     return json_text
 
-def send_result():
+def send_result(code):
     header = {'Content-Type': 'application/json;charset=utf-8'}
     bot_url_risk = "https://open.feishu.cn/open-apis/bot/v2/hook/f85d86c8-69c6-48c8-876e-2351b9c8bae2"
     retry_time = 5
     for _ in range(retry_time):
         try:
-            requests.post(bot_url_risk, json = gen_json_text(), headers=header)
+            requests.post(bot_url_risk, json = gen_json_text(code), headers=header)
             break
         except Exception as e:
             print("create feishu tag failed, reason is {}".format(e))
             time.sleep(5)
 
-send_result()
+send_result("123456")
