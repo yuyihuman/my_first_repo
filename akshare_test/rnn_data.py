ac0ae8e10a6f8666dbbfb2f4249cc466884d16d5
YuYi <yuyi.human@163.com>
2024-10-09 15:11:47 +0800

    update some data

--- akshare_test/rnn_data.py
+++ akshare_test/rnn_data.py
@@ -4,5 +4,5 @@ import akshare as ak
 # stock_zh_a_spot_em_df.to_csv('output.csv', index=False)
 
 stock_index = "000001"
-stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=stock_index, period="daily", start_date="20170301", end_date='20230907', adjust="")
+stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=stock_index, period="daily", start_date="20170301", end_date='20240907', adjust="")
 stock_zh_a_hist_df.to_csv(stock_index+'.csv', index=False)
