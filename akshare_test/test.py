61ea832f8f0371466f8404815ed011e2b1f3dc0e
YuYi <yuyi.human@163.com>
2025-04-25 08:08:03 +0800

    add shfj

--- akshare_test/test.py
+++ akshare_test/test.py
@@ -1,4 +1,4 @@
 import akshare as ak
 
-stock_financial_cash_ths_df = ak.stock_financial_cash_ths(symbol="000063", indicator="按单季度")
-print(stock_financial_cash_ths_df.info())
+macro_china_enterprise_boom_index_df = ak.macro_china_enterprise_boom_index()
+print(macro_china_enterprise_boom_index_df)
