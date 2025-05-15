7f6f387489ef822b18ceda509308e288b9350992
YuYi <yuyi.human@163.com>
2025-05-15 21:08:17 +0800

    add script

--- stockapi/run_stock_analysis.bat
+++ stockapi/run_stock_analysis.bat
@@ -1,6 +1,6 @@
 @echo off
-del /q C:\Users\Ramsey\github\my_first_repo\stockapi\logs\*.log
-echo 开始执行股票数据分析任务 - %date% %time% >> C:\Users\Ramsey\github\my_first_repo\stockapi\logs\schedule_log.txt
-cd /d C:\Users\Ramsey\github\my_first_repo\stockapi
-python run_stock_analysis.py >> C:\Users\Ramsey\github\my_first_repo\stockapi\logs\schedule_log.txt 2>&1
-echo 任务执行完成 - %date% %time% >> C:\Users\Ramsey\github\my_first_repo\stockapi\logs\schedule_log.txt
+cd /d %~dp0
+del /q logs\*.log
+echo 开始执行股票数据分析任务 - %date% %time% >> logs\schedule_log.txt
+python run_stock_analysis.py >> logs\schedule_log.txt 2>&1
+echo 任务执行完成 - %date% %time% >> logs\schedule_log.txt
