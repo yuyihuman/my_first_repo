cb8c0a0247c35ee1191c340eb7a6f38a0468a57e
Ramsey <17701825950@163.com>
2025-08-14 00:10:03 +0800

    change process

--- stockapi/commodity_price_index.py
+++ stockapi/commodity_price_index.py
@@ -34,7 +34,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(current_log_file),
+        logging.FileHandler(current_log_file, encoding='utf-8'),
         logging.StreamHandler()
     ]
 )
