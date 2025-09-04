ab2fe68e60a1ae6eed038b03594bf808ace719de
Ramsey <17701825950@163.com>
2025-09-04 20:29:31 +0800

    change process

--- stockapi/run_quarterly_analysis.bat
+++ stockapi/run_quarterly_analysis.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
+python stock_base_info\get_all_stocks_data.py
 python commodity_price_index.py
 python true_quarterly_analysis.py
 python pmi_data_fetcher.py
-python stock_base_info\get_all_stocks_data.py
